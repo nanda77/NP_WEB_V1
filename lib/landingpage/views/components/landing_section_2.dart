d394835818521d3cd4321f50384f05f0086bee13
nanda77 <nalandaneeraj.n7@gmail.com>
2022-01-25 13:39:13 +0530

    d3

--- lib/landingpage/views/components/landing_section_2.dart
+++ lib/landingpage/views/components/landing_section_2.dart
@@ -1,210 +1,67 @@
 import 'dart:ui';
 
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ninjapay/landingpage/views/components/url_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../constants.dart';
 
-class LandingSection2 extends StatefulWidget {
-  @override
-  _LandingSection2State createState() => _LandingSection2State();
-}
-
-class _LandingSection2State extends State<LandingSection2> {
-  CarouselController buttonCarouselController = CarouselController();
-  int _current = 0;
-  List imgList = [
-    'assets/images/add_project.gif', //project_creation //https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60 //assets/images/add_project.gif
-    'assets/images/add_work.gif', //add_work_title
-    'assets/images/quick_update.gif', //add_update //normal_update //quick_update
-    'assets/images/reports_signed.gif', //
-    'assets/images/add_material.gif', //material
-    'assets/images/add_usage.gif', //material_wt
-    'assets/images/360_add.gif', //360
-    'assets/images/attendance.gif',
-    'assets/images/location.gif'
-  ];
-  List<String> stringList1 = [
-    'Step 1', //
-    'Step 2',
-    'Step 3',
-    'Step 4',
-    'Step 5',
-    'Step 6',
-    'Step 7',
-    'Step 8',
-    'Step 9'
-  ];
-  List<String> stringList2 = [
-    'How to create a project?', //
-    'How to add works under a project?',
-    'Adding daily updates under a work?',
-    'Downloading signed pdf work reports?',
-    'How to add materials under a project?',
-    'Linking material usage to a work?',
-    'Adding a 360° image of the site?',
-    'Maintaining staff attendance?',
-    "Tracking staff's location on site?"
-  ];
-  List<T> map<T>(List list, Function handler) {
-    List<T> result = [];
-    for (var i = 0; i < list.length; i++) {
-      result.add(handler(i, list[i]));
-    }
-    return result;
-  }
-
+class LandingSection3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      constraints: BoxConstraints(maxHeight: 720, minHeight: 500),
+      constraints: BoxConstraints(maxHeight: 620, minHeight: 500),
       margin: EdgeInsets.only(left: 100, right: 100, top: 50),
+      // width: double.infinity,
+      // decoration: BoxDecoration(
+      //   image: DecorationImage(
+      //     fit: BoxFit.fill,
+      //     image: AssetImage("assets/images/bg3.png"),
+      //   ),
+      // ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
         children: [
+          Image.asset(
+            'assets/images/s2.png',
+          ),
           Expanded(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  alignment: Alignment.bottomLeft,
-                  child: Text(
-                    "${stringList1[_current]}",
-                    style: GoogleFonts.montserrat(
-                        fontSize: 42,
-                        fontWeight: FontWeight.w900,
-                        color: Color(0xFF282F3F)),
-                  ),
+                Image.asset(
+                  'assets/icons/t2.svg',
                 ),
                 Container(
-                  child: Text(
-                    "${stringList2[_current]}",
-                    style: GoogleFonts.montserrat(
-                        fontSize: 42,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF282F3F)),
+                  margin: EdgeInsets.only(left: 50),
+                  child: Row(
+                    children: [
+                      TextButton.icon(
+                        icon: Image.asset("assets/images/play_store.png",
+                            height: 57),
+                        label: Text(""),
+                        onPressed: StoreUrls().playStoreURL,
+                      ),
+                      TextButton.icon(
+                        icon: Image.asset("assets/images/app_store.png",
+                            height: 57),
+                        label: Text(""),
+                        onPressed: StoreUrls()
+                            .appStoreURL, // https://apps.apple.com/in/app/ninjapay-manage-construction/id1547753533
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: map<Widget>(imgList, (index, url) {
-                    return Container(
-                      width: 7.0,
-                      height: 7.0,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: _current == index
-                            ? Colors.deepPurple
-                            : Colors.black38,
-                      ),
-                    );
-                  }),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    OutlinedButton(
-                      onPressed: goToPrevious,
-                      child: Text(
-                        "<",
-                        style:
-                            TextStyle(color: Colors.deepPurple, fontSize: 27),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    OutlinedButton(
-                      onPressed: goToNext,
-                      child: Text(
-                        ">",
-                        style:
-                            TextStyle(color: Colors.deepPurple, fontSize: 27),
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              SizedBox(
-                width: 500,
-                height: 720,
-                child: CarouselSlider(
-                    carouselController: buttonCarouselController,
-                    options: CarouselOptions(
-                      height: 720,
-                      initialPage: 0,
-                      enlargeCenterPage: true,
-                      enableInfiniteScroll: true,
-                      autoPlay: false,
-                      autoPlayInterval: Duration(seconds: 60),
-                      autoPlayAnimationDuration: Duration(milliseconds: 2000),
-                      reverse: false,
-                      pauseAutoPlayOnTouch: true,
-                      scrollDirection: Axis.horizontal,
-                      onPageChanged: (index, reason) {
-                        setState(() {
-                          _current = index;
-                        });
-                      },
-                    ),
-                    items: imgList.map((imgUrl) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 10),
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(width: 2, color: Colors.black),
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(20)),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.network(
-                                imgUrl,
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }).toList()),
-              ),
-            ],
-          ),
         ],
       ),
     );
-  }
-
-  goToPrevious() {
-    buttonCarouselController.previousPage(
-        duration: Duration(milliseconds: 300), curve: Curves.ease);
-  }
-
-  goToNext() {
-    buttonCarouselController.nextPage(
-        duration: Duration(milliseconds: 300), curve: Curves.decelerate);
   }
 }
